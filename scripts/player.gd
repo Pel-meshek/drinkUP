bbbdb3e3d32fe8ded0123a8010d3734c14fae1a5
Pel-meshek <YA07DA@yandex.ru>
2026-05-01 13:47:48 +0300

    man

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,11 +11,21 @@ func _physics_process(delta: float) -> void:
 	var direction_x := Input.get_axis("left", "right")
 	if direction_x:
 		velocity.x = direction_x * SPEED
+		$AnimatedSprite2D.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.flip_h = false
 	var direction_y := Input.get_axis("up","down")
-	if direction_y:
+	if direction_y >= 0:
 		velocity.y = direction_y * SPEED
+		$AnimatedSprite2D.flip_v = true
+	elif direction_y < 0:
+		velocity.y = direction_y * SPEED
+		$AnimatedSprite2D.flip_v = false
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+	if direction_x or direction_y:
+		$AnimatedSprite2D.play("walk")
+	else:
+		$AnimatedSprite2D.play("idle")
 	move_and_slide()
